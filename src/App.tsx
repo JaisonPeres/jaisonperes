feb3e609d509c14b9bb4bdbbe4409b5874125446
Jaison Peres <jaison.peres@payface.com.br>
2023-04-11 00:06:11 -0300

    fix: remove logos and add linkedin link
    Signed-off-by: Jaison Peres <jaison.peres@payface.com.br>

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,14 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Jaison Peres</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Em breve, mais informações sobre mim.
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        <a href='https://www.linkedin.com/in/jaison-cperes/' target='_blank'>linkedin.com/in/jaison-cperes</a>
       </p>
     </div>
   )
